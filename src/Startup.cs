48d9ca7cb71d71f98b43c00f26389a9cc818a2ea
MBerthold <MBerthold@NB-FC-215335>
2020-03-26 08:17:36 +0100

    cleanup usings

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,18 +1,8 @@
-#region License
-/*
-Copyright (c) 2019 Konrad Mattheis und Martin Berthold
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-#endregion
-
 namespace Ser.Engine.Rest
 {
     #region Usings
     using System;
     using System.IO;
-    using System.Linq;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -22,15 +12,11 @@ namespace Ser.Engine.Rest
     using Microsoft.OpenApi.Models;
     using System.Collections.Generic;
     using NLog;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc.ModelBinding;
     using Microsoft.Extensions.Hosting;
     using Ser.Engine.Rest.Services;
-    using Microsoft.Extensions.Options;
     using Microsoft.AspNetCore.Rewrite;
-    using System.Net;
     using Prometheus;
-    using System.Text;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Server.Kestrel.Core;
     #endregion
