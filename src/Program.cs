ff09058bbb2bee6b7522bc9ea2ca5cfda1b7ab1b
MBerthold <MBerthold@NB-FC-215335>
2021-05-28 07:06:30 +0200

    Change to Service

--- src/Program.cs
+++ src/Program.cs
@@ -2,13 +2,11 @@
 {
     #region Usings
     using System;
-    using System.IO;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Logging;
     using NLog;
     using NLog.Web;
+    using PeterKottas.DotNetCore.WindowsService;
     #endregion
 
     /// <summary>
@@ -32,34 +30,42 @@
                 //Activate Nlog logger with configuration
                 logger = NLogBuilder.ConfigureNLog("App.config").GetCurrentClassLogger();
 
-                //Build config for webserver
-                var config = new ConfigurationBuilder()
-                    .SetBasePath(AppContext.BaseDirectory)
-                    .AddEnvironmentVariables()
-                    .AddCommandLine(args)
-                    .Build();
+                // Use as Windows Service
+                ServiceRunner<WebService>.Run(config =>
+                {
+                    config.SetDisplayName("AnalyticsGate Rest Service");
+                    config.SetDescription("Rest Service for AnalyticsGate Reporting");
+                    var name = config.GetDefaultName();
+                    config.Service(serviceConfig =>
+                    {
+                        serviceConfig.ServiceFactory((extraArguments, controller) =>
+                        {
+                            var webService = new WebService(args);
+                            return webService;
+                        });
+                        serviceConfig.OnStart((service, extraParams) =>
+                        {
+                            logger.Debug($"Service {name} started...");
+                            service.Start();
+                        });
+                        serviceConfig.OnStop(service =>
+                        {
+                            logger.Debug($"Service {name} stopped...");
+                            service.Stop();
+                        });
+                        serviceConfig.OnError(ex =>
+                        {
+                            logger.Error($"Service Exception: {ex}");
+                        });
+                    });
+                });
 
-                //Start the web server
-                CreateHostBuilder(args)
-                    .UseKestrel()
-                    .ConfigureAppConfiguration((builderContext, config) =>
-                    {
-                        config.AddJsonFile("appsettings.json", optional: false);
-                    })
-                    .UseConfiguration(config)
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .ConfigureLogging(logging =>
-                    {
-                        logging.ClearProviders();
-                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                    })
-                    .UseNLog()
-                    .Build()
-                    .Run();
+                Environment.Exit(0);
             }
             catch (Exception ex)
             {
-                logger.Error(ex, "AG Rest Service has fatal error.");
+                logger.Error(ex, "Cloud connector service has fatal error.");
+                Environment.Exit(1);
             }
             finally
             {
---
+++ src/WebService.cs
@@ -0,0 +1,116 @@
+﻿namespace Ser.Engine.Rest
+{
+    #region Usings
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using NLog;
+    using NLog.Web;
+    using PeterKottas.DotNetCore.WindowsService.Base;
+    using PeterKottas.DotNetCore.WindowsService.Interfaces;
+    #endregion
+
+    /// <summary>
+    /// Web service class
+    /// </summary>
+    public class WebService : MicroService, IMicroService
+    {
+        #region Logger
+        private readonly static Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region Properties && Variables
+        private static CancellationTokenSource cts = new CancellationTokenSource();
+        /// <summary>
+        /// CMD Arguments
+        /// </summary>
+        public static string[] Arguments { get; private set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// web service constructor
+        /// </summary>
+        /// <param name="args">cmd arguments</param>
+        public WebService(string[] args)
+        {
+            Arguments = args;
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Create a new web host builder
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        /// <returns>WebHostBuilder instance</returns>
+        private static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseStartup<Startup>();
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Start windows service
+        /// </summary>
+        public void Start()
+        {
+            try
+            {
+                Task.Run(() =>
+                {
+                    //Build config for webserver
+                    var config = new ConfigurationBuilder()
+                        .SetBasePath(AppContext.BaseDirectory)
+                        .AddEnvironmentVariables()
+                        .AddCommandLine(Arguments)
+                        .Build();
+
+                    //Start the web server
+                    CreateHostBuilder(Arguments)
+                         .UseKestrel()
+                         .ConfigureAppConfiguration((builderContext, config) =>
+                         {
+                             config.AddJsonFile("appsettings.json", optional: false);
+                         })
+                         .UseConfiguration(config)
+                         .UseContentRoot(Directory.GetCurrentDirectory())
+                         .ConfigureLogging(logging =>
+                         {
+                             logging.ClearProviders();
+                             logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                         })
+                         .UseNLog()
+                         .Build()
+                         .Run();
+                }, cts.Token);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Web service startup failed.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Stop windows service
+        /// </summary>
+        public void Stop()
+        {
+            try
+            {
+                logger.Info("Service was stopped...");
+                cts.Cancel();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Web service stopping failed.", ex);
+            }
+        }
+        #endregion
+    }
+}
